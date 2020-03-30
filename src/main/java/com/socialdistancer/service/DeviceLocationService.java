b31b4f718bf1f4c9483f359552037360e1b32529
Arun Sabtharishi <arun.sabtharishi@gmail.com>
2020-03-30 11:31:59 -0500

    Create DeviceLocationService.java

---
+++ src/main/java/com/socialdistancer/service/DeviceLocationService.java
@@ -0,0 +1,7 @@
+package com.socialdistancer.service;
+
+import com.socialdistancer.model.LocationDetails;
+
+public interface DeviceLocationService {
+    void updateDeviceLocation(LocationDetails locationDetails);
+}
